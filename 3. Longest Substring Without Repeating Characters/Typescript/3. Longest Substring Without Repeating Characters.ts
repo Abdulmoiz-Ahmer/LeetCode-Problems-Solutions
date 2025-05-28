c8d164af67b4575467cd2c931386a1e0c6fb7956
Abah-Ciklum <abah@ciklum.com>
2025-05-28 15:10:45 +0500

    ADDS: solution of 3. Longest Substring Without Repeating Characters

---
+++ 3. Longest Substring Without Repeating Characters/Typescript/3. Longest Substring Without Repeating Characters.ts
@@ -0,0 +1,17 @@
+function lengthOfLongestSubstring(s) {
+    let start = 0;
+    let maxLength = 0;
+    const lastSeen = new Map();
+  
+    for (let end = 0; end < s.length; end++) {
+      const char = s[end];
+      if (lastSeen.has(char) && lastSeen.get(char) >= start) {
+        start = lastSeen.get(char) + 1;
+      }
+      lastSeen.set(char, end);
+      maxLength = Math.max(maxLength, end - start + 1);
+    }
+  
+    return maxLength;
+  }
+  
